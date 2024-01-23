0f33f0e3592356c7c43b16b46e72e5903888a920
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2024-01-23 14:18:13 -0600

    Sistema medicos
    Creacion de men

---
+++ SistemasMedicos/app.py
@@ -0,0 +1,27 @@
+#importacion del framework
+from flask import Flask, request, session, render_template, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+
+#Inicializacion del APP
+app = Flask(__name__, static_folder='public', template_folder='templates')
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='SistemasMedicos'
+app.secret_key= 'mysecrety'
+mysql= MySQL(app)
+
+#Declaracion de ruta http://localhost:5000
+@app.route('/')
+def bienvenida():
+  
+  return render_template('bienvenida.html')
+
+
+
+
+
+#Ejecucion del servidor en el puerto 5000
+if __name__ == '__main__':
+  app.run(port=1000,debug=True)
