16312f86c0a2ad08b1919dd7fa38cfd308cd0496
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2024-01-26 10:43:12 -0600

    Arreglos

--- SistemasMedicos/app.py
+++ SistemasMedicos/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__, static_folder='public', template_folder='templates')
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='consultasmed'
+app.config['MYSQL_DB']='SistemasMedicos'
 app.secret_key= 'mysecrety'
 mysql= MySQL(app)
 
