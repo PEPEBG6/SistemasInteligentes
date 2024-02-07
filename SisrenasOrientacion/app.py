3c14727d6287da4c1727162203a60dd692b9ffaf
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2024-02-07 12:22:25 -0600

    SistemaOrientacion

--- SisrenasOrientacion/app.py
+++ SisrenasOrientacion/app.py
@@ -8,13 +8,74 @@ app = Flask(__name__, static_folder='public', template_folder='templates')
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
 app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='sistemaOrientacion'
+app.config['MYSQL_DB']='sistema_orientacion'
 app.secret_key= 'mysecrety'
 mysql= MySQL(app)
 
-@app.route('/')
-def bienvenida():
-  return render_template('index.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        matricula = request.form['student_id']
+        CC = mysql.connection.cursor()
+        CC.execute('''SELECT e.id AS Matricula, e.nombre AS Nombre, m.materia AS Materia, 
+                            MAX(CASE WHEN p.parcial = 'Parcial 1' THEN ca.calificacion END) AS Parcial1,
+                            MAX(CASE WHEN p.parcial = 'Parcial 2' THEN ca.calificacion END) AS Parcial2,
+                            MAX(CASE WHEN p.parcial = 'Parcial 3' THEN ca.calificacion END) AS Parcial3
+                      FROM cargas_academicas AS ca
+                      INNER JOIN cargas_estudiantes AS ce ON ca.id_carga_estudiante = ce.id
+                      INNER JOIN estudiantes AS e ON ce.id_estudiante = e.id
+                      INNER JOIN materias AS m ON ca.id_materia = m.id
+                      INNER JOIN parciales AS p ON ca.id_parcial = p.id
+                      WHERE e.id = %s
+                      GROUP BY e.id, e.nombre, m.materia''', (matricula,))
+        conalum = CC.fetchall()
+        
+        # Calcular el estado del alumno
+        estado_alumno = calcular_estado_alumno(conalum)
+        
+        return render_template('index.html', listalum=conalum, estado_alumno=estado_alumno)
+    else:
+        return render_template('index.html')
+    
+
+def calcular_estado_alumno(conalum):
+    # Obtener las calificaciones de los parciales
+    calificaciones = [carga[3:6] for carga in conalum]
+    # Lista para almacenar el estado de aprobación de cada materia
+    materias_aprobadas = []
+    
+    for carga in calificaciones:
+        # Verificar si todas las calificaciones son mayores o iguales a 7.0 en cada materia
+        aprobada = all(calificacion >= 7.0 for calificacion in carga)
+        materias_aprobadas.append(aprobada)
+    
+    # Contar el número de materias aprobadas
+    num_materias_aprobadas = sum(materias_aprobadas)
+    num_materias_totales = len(materias_aprobadas)
+    
+    # Verificar si el alumno tiene menos de la mitad de materias aprobadas y alguna calificación menor a 7.0
+    if num_materias_aprobadas < num_materias_totales / 2 and any(any(calificacion < 7.0 for calificacion in carga) for carga in calificaciones):
+        return "Baja Académica"
+    # Verificar si todas las calificaciones son mayores o iguales a 7.0
+    elif all(all(calificacion >= 7.0 for calificacion in carga) for carga in calificaciones):
+        return "Exento"
+    # Verificar si al menos una calificación es mayor o igual a 7.0 y las otras dos son menores a 7.0
+    elif any(any(calificacion >= 7.0 for calificacion in carga) for carga in calificaciones):
+        # Contar la cantidad de parciales aprobados
+        aprobados = sum(any(calificacion >= 7.0 for calificacion in carga) for carga in calificaciones)
+        # Verificar si el alumno puede realizar finales
+        if aprobados < 3:
+            return "Tendrá Finales"
+        else:
+            return "Baja Academica"
+    else:
+        return "Indefinido"
+
+
+
+
+
 
 
 
--- SisrenasOrientacion/db_sistemaOrientacion.sql
+++ SisrenasOrientacion/db_sistemaOrientacion.sql
@@ -11,30 +11,83 @@ ap varchar(100),
 am varchar(100)
 );
 
+insert into estudiantes(nombre,ap,am)
+values
+('Jose Luis','Bernardo','Gutierrez'),
+('Jose Andres','Hernadez','Valdez'),
+('Jose Alberto','Gomez','Utrera'),
+('Juan Pablo','Perez','Valadez'),
+('Maria Monserrat','Campuzano','Leon'),
+('Paola','Duran','Castillo'),
+('Sofia','Garcia','Perez'),
+('Johan Israel','Mauricio','Hernandez'),
+('Diego','Reyes','Pedraza'),
+('Alvaro','Zuñiga','Torres');
+
 create table materias(
 id int not null primary key auto_increment,
 materia varchar(100)
 );
+insert into materias(materia)
+values
+('Matematicas'),
+('Español'),
+('Física'),
+('Redes'),
+('Base de Datos'),
+('Programación'),
+('Habilidades Gerenciales'),
+('Gestion de Proyectos'),
+('Filosofia'),
+('Ingles');
 
 create table carreras(
 id int not null primary key auto_increment,
 carrera varchar(100)
 );
+insert into carreras(carrera)
+values
+('Negocios'),
+('Administracion de Empresas'),
+('Automotriz'),
+('Mecatronica'),
+('Manufactura'),
+('Sistemas'),
+('Telecomunicaciones');
 
 create table grupos(
 id int not null primary key auto_increment,
 grupo varchar(100)
 );
+insert into grupos(grupo)
+values
+('N181'),
+('AE181'),
+('A181'),
+('ME181'),
+('MA181'),
+('S181'),
+('T181');
 
 create table periodos(
 id int not null primary key auto_increment,
 periodo varchar(100)
 );
+insert into periodos(periodo)
+values
+('E-A'),
+('M-A'),
+('S-D');
 
 create table parciales(
 id int not null primary key auto_increment,
 parcial varchar(100)
 );
+insert into parciales(parcial)
+values
+('Parcial 1'),
+('Parcial 2'),
+('Parcial 3');
 
 create table cargas_estudiantes(
 id int not null primary key auto_increment,
@@ -47,6 +100,20 @@ foreign key (id_carrera) references carreras (id) on delete cascade on update ca
 foreign key (id_grupo) references grupos (id) on delete cascade on update cascade,
 foreign key (id_periodo) references periodos (id) on delete cascade on update cascade
 );
+insert into cargas_estudiantes(id_estudiante, id_carrera, id_grupo, id_periodo)
+values
+(1,1,1,1),
+(2,2,2,1),
+(3,3,3,1),
+(4,4,4,1),
+(5,5,5,1),
+(6,6,6,1),
+(7,7,7,1),
+(8,1,1,1),
+(9,2,2,1),
+(10,3,3,1);
+
+select * from cargas_estudiantes;
 
 create table cargas_academicas(
 id int not null primary key auto_increment,
@@ -58,3 +125,94 @@ foreign key (id_carga_Estudiante) references cargas_estudiantes (id) on delete c
 foreign key (id_materia) references materias (id) on delete cascade on update cascade,
 foreign key (id_parcial) references parciales (id) on delete cascade on update cascade
 );
+insert into cargas_academicas(id_carga_estudiante, id_materia,id_parcial,calificacion)
+values
+(1,1,1,'8.5'),
+(1,1,2,'7.5'),
+(1,1,3,'6.3'),
+
+(1,2,1,'7.5'),
+(1,2,2,'7.0'),
+(1,2,3,'8.3'),
+
+(1,3,1,'10.0'),
+(1,3,2,'9.5'),
+(1,3,3,'9.0'),
+
+(1,4,1,'6.5'),
+(1,4,2,'6.9'),
+(1,4,3,'6.8'),
+
+(1,5,1,'8.0'),
+(1,5,2,'7.9'),
+(1,5,3,'9.1'),
+
+(1,6,1,'6.5'),
+(1,6,2,'5.8'),
+(1,6,3,'7.1'),
+
+(1,7,1,'8.0'),
+(1,7,2,'8.2'),
+(1,7,3,'8.3'),
+
+(2,1,1,'8.5'),
+(2,1,2,'8.5'),
+(2,1,3,'8.3'),
+(2,2,1,'7.5'),
+(2,2,2,'7.0'),
+(2,2,3,'8.3'),
+(2,3,1,'10.0'),
+(2,3,2,'9.5'),
+(2,3,3,'9.0'),
+(2,4,1,'8.5'),
+(2,4,2,'9.9'),
+(2,4,3,'7.8'),
+(2,5,1,'8.0'),
+(2,5,2,'7.9'),
+(2,5,3,'9.1'),
+(2,6,1,'8.5'),
+(2,6,2,'7.8'),
+(2,6,3,'7.1'),
+(2,7,1,'8.0'),
+(2,7,2,'8.2'),
+(2,7,3,'8.3'),
+
+(3,1,1,'6.5'),
+(3,1,2,'5.5'),
+(3,1,3,'6.3'),
+(3,2,1,'5.5'),
+(3,2,2,'4.0'),
+(3,2,3,'3.3'),
+(3,3,1,'5.0'),
+(3,3,2,'6.5'),
+(3,3,3,'6.0'),
+(3,4,1,'5.5'),
+(3,4,2,'7.9'),
+(3,4,3,'5.8'),
+(3,5,1,'6.0'),
+(3,5,2,'4.9'),
+(3,5,3,'3.1'),
+(3,6,1,'7.5'),
+(3,6,2,'6.8'),
+(3,6,3,'7.1'),
+(3,7,1,'4.0'),
+(3,7,2,'5.2'),
+(3,7,3,'6.3');
+
+        
+select * from cargas_academicas;
+select * from cargas_estudiantes;
+select * from estudiantes;
+select * from materias;
+select * from parciales;
+
+select e.id as Matricula, e.nombre as Nombre, m.materia as Materia, p.parcial as Parcial, ca.calificacion as Calificacion from cargas_academicas as ca
+inner join cargas_estudiantes as ce
+on ca.id_carga_Estudiante=ce.id
+inner join estudiantes as e
+on ce.id_estudiante=e.id
+inner join materias as m
+on ca.id_materia=m.id
+inner join parciales as p
+on ca.id_parcial=p.id
+where e.id=1;
